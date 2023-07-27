f67ddfff88c3daae209077e0cd2331a8cae96493
Vlad.Zherko <vladikon19@tut.by>
2023-07-27 14:35:28 +0300

    Lesson 9 - regular expression added

--- Lesson9-strings/src/main/java/HomeWork/Main.java
+++ Lesson9-strings/src/main/java/HomeWork/Main.java
@@ -4,6 +4,7 @@ import HomeWork.StringMethods;
 
 public class Main {
     public static void main(String[] args) {
+        // Без ввода, решил готовую строку передавать, долго ввводить
         String code = "5555-ABC-5678-DeF-9g10k";
 
         StringMethods.showTwoFirstNumberBlocks(code);
