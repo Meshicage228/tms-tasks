4d4a8b6b1f4263a95ba427ca392632c18f5c0408
Vlad.Zherko <vladikon19@tut.by>
2023-07-30 18:38:54 +0300

    Lesson 9 - Try to work with notepad

---
+++ Lesson9-strings/src/main/java/Additional/ModuleEngine.java
@@ -0,0 +1,44 @@
+package Additional;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+
+public class ModuleEngine {
+
+    public static void main(String[] args) throws IOException, InterruptedException{
+        // Та же проблема не знаю как загрузить уже скомпилированный файл
+        String pathFile = "D:/JavaProjects/tms-tasks/Lesson9-strings/src/main/java/Additional/myfile.java";
+
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec("notepad.exe " + pathFile);
+        process.waitFor();
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        compiler.run(null, null, null, pathFile);
+
+        try {
+            URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{new URL("file:/D:/JavaProjects/tms-tasks/Lesson9-strings/src/main/java/Additional/myfile")});
+            Class<?> myClass = urlClassLoader.loadClass("Additional.myfile"); // Тут грузит старое ( с прошлой компиляции)
+
+            Method[] classMethods =  myClass.getDeclaredMethods();
+
+            System.out.println(Arrays.toString(classMethods));
+
+            classMethods[1].invoke(myClass);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            System.out.println("Cannot invoke");
+        }
+
+
+    }
+
+}
--- Lesson9-strings/src/main/java/Additional/myfile.java
+++ Lesson9-strings/src/main/java/Additional/myfile.java
@@ -1,9 +1,12 @@
-public class myfile {
-    public static void main(String[] args) {
-        show();
-
+package Additional;
+public class myfile{
+	public static void main(String[] args){
+	show();
+	}
+	public static void show() {
+        System.out.println("hello!");
     }
-    public static void show(){
-	System.out.println("hello!");
+public static void show123() {
+        System.out.println("hello!");
     }
 }
