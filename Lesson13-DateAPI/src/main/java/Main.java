24400a497d953e0c5f1d18fa193eaa1c5bc6987e
Vlad.Zherko <vladikon19@tut.by>
2023-08-17 15:49:07 +0300

    Lesson13 - DateAPI

--- Lesson13-DateAPI/src/main/java/Main.java
+++ Lesson13-DateAPI/src/main/java/Main.java
@@ -1,16 +1,9 @@
 import additional.TaskOneMethod;
 
-import java.sql.SQLOutput;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.spi.LocaleServiceProvider;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,7 +22,7 @@ public class Main {
 
         //2.
         LocalDate localDate = LocalDate.now();
-        switch (localDate.getDayOfWeek()){
+        switch (localDate.getDayOfWeek()) {
             case MONDAY -> System.out.println(localDate.plusDays(8));
             case TUESDAY -> System.out.println(localDate.plusDays(7));
             case WEDNESDAY -> System.out.println(localDate.plusDays(6));
--- Lesson13-DateAPI/src/main/java/mainTask/MainTask.java
+++
@@ -1,4 +0,0 @@
-package mainTask;
-
-public class MainTask {
-}
