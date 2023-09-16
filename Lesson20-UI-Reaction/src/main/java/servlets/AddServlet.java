7080f1e922fe12b831d6f74ac2d7279776a8c1de
Vlad.Zherko <vladikon19@tut.by>
2023-09-16 14:09:38 +0300

    added filter to add servlet

--- Lesson20-UI-Reaction/src/main/java/servlets/AddServlet.java
+++ Lesson20-UI-Reaction/src/main/java/servlets/AddServlet.java
@@ -9,23 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 @WebServlet("/add")
 public class AddServlet extends HttpServlet {
     private final CustomDB customDB = new CustomDB();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        List<String> spaces = parameterMap.entrySet().stream()
-                .flatMap(entry -> Arrays.stream(entry.getValue()))
-                .filter(String::isEmpty)
-                .toList();
-
-        if(spaces.isEmpty()) {
-
             String title = req.getParameter("title");
             float height = Float.parseFloat(req.getParameter("height"));
             float weight = Float.parseFloat(req.getParameter("weight"));
@@ -37,8 +26,6 @@ public class AddServlet extends HttpServlet {
             fridge.setWeight(weight);
 
             customDB.create(fridge);
-        }
-
         req.getRequestDispatcher("/home").forward(req, resp);
     }
 }
