e63ab8eb980c90c93f9bf16c0c1ecd8d6b614749
Vlad.Zherko <vladikon19@tut.by>
2023-09-16 14:03:26 +0300

    init

---
+++ Lesson21-UI-JSTL/src/main/java/servlets/addFilter.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@WebFilter("/add")
+public class addFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        List<String> spaces = parameterMap.entrySet().stream()
+                .flatMap(entry -> Arrays.stream(entry.getValue()))
+                .filter(String::isEmpty)
+                .toList();
+
+        if(spaces.isEmpty()){
+            chain.doFilter(request,response);
+            request.setAttribute("toshow", false);
+        }
+        else{
+            request.setAttribute("toshow", true);
+            request.getRequestDispatcher("/home").forward(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
