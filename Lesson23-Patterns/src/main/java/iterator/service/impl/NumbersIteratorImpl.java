8a63c34e2f3c3652458be1e95e6d197a97e07828
Vlad.Zherko <vladikon19@tut.by>
2023-09-24 17:26:45 +0300

    simplified code

--- Lesson23-Patterns/src/main/java/iterator/service/impl/NumbersIteratorImpl.java
+++ Lesson23-Patterns/src/main/java/iterator/service/impl/NumbersIteratorImpl.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class NumbersIteratorImpl<T> implements Iterator<T> {
     private final List<T> entity;
-    private int idx = -1;
+    private int idx = 0;
 
     public NumbersIteratorImpl(List<T> entity) {
         this.entity = entity;
@@ -14,29 +14,16 @@ public class NumbersIteratorImpl<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
-        return findNextIdx() != -2;
+        return idx < entity.size();
     }
 
     @Override
     public T next() {
-        idx = findNextIdx();
-        if (idx != -2) {
-            return entity.get(idx);
+        if (!hasNext()) {
+            return null;
         }
-        return null;
-    }
-    private int findNextIdx() {
-        var tempIdx = idx;
-        while (true) {
-            tempIdx++;
-            if (tempIdx >= entity.size()) {
-                tempIdx = -2;
-                break;
-            }
-            if (entity.get(tempIdx) != null) {
-                break;
-            }
-        }
-        return tempIdx;
+        T el = entity.get(idx);
+        idx++;
+        return el;
     }
 }
