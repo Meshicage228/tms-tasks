83c3daf52f0b4faa69a6fd78b8127196256a6c99
Vlad.Zherko <vladikon19@tut.by>
2023-10-05 18:30:37 +0300

    created config package

---
+++ Lesson26-JavaConfigAndAnnotation/src/main/java/horseRacing/com/config/AppConfig.java
@@ -0,0 +1,21 @@
+package horseRacing.com.config;
+
+import horseRacing.com.domain.*;
+import horseRacing.com.services.impl.CoupleServiceImpl;
+import horseRacing.com.services.impl.WalletServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+
+@Configuration
+@ComponentScan(basePackages = {"horseRacing.com"})
+public class AppConfig {
+    @Bean
+    @Scope("prototype")
+    RacingGame racingGame(WalletServiceImpl walletService, CoupleServiceImpl coupleService) {
+        return new RacingGame(walletService, coupleService);
+    }
+
+}
