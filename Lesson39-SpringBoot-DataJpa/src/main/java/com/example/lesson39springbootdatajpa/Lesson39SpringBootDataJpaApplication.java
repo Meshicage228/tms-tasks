a6465883f39b5c6d48a1eb9c8e01404bd8b474dc
Vlad.Zherko <vladikon19@tut.by>
2023-11-09 16:57:35 +0300

    init

---
+++ Lesson39-SpringBoot-DataJpa/src/main/java/com/example/lesson39springbootdatajpa/Lesson39SpringBootDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lesson39springbootdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson39SpringBootDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson39SpringBootDataJpaApplication.class, args);
+    }
+
+}
---
+++ Lesson39-SpringBoot-DataJpa/src/test/java/com/example/lesson39springbootdatajpa/Lesson39SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lesson39springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson39SpringBootDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
