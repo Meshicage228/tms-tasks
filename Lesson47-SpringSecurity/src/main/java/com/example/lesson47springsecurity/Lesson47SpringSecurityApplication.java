802d4c3597f7a4374100a04403072fa71a1b57e6
Vlad.Zherko <vladikon19@tut.by>
2023-12-23 23:50:38 +0300

    init

---
+++ Lesson47-SpringSecurity/src/main/java/com/example/lesson47springsecurity/Lesson47SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.lesson47springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson47SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson47SpringSecurityApplication.class, args);
+    }
+
+}
